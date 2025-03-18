e6bb714dce816e4eea1b54189cfb52e8ec158f50
NaellyV <naellyvitoria8@gmail.com>
2025-03-17 21:59:04 -0300

    feat: terminando a tela inicial

--- js/script.js
+++ js/script.js
@@ -10,22 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Seleciona elementos do DOM
 const openModalBtn = document.getElementById("open-modal-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
 const modal = document.getElementById("contact-modal");
 
-// Abre o modal
 openModalBtn.addEventListener("click", () => {
     modal.style.display = "flex";
 });
 
-// Fecha o modal ao clicar no botão de fechar
 closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Fecha o modal ao clicar fora da área do modal
 window.addEventListener("click", (event) => {
     if (event.target === modal) {
         modal.style.display = "none";
